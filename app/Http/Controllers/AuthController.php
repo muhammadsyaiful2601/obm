4d92d87883b2557704875fc535cfb21ee69e78e0
Udingamingcd <muhammadsyaiful2601@gmail.com>
2026-04-29 09:56:16 +0700

    fiks sedikit walaupun pusing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Services\AuthService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
 {
     protected $authService;
 
-    // public function __construct(AuthService $authService)
-    // {
-    //     $this->authService = $authService;
-    // }
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
 
     public function index()
     {
@@ -29,31 +28,66 @@ class AuthController extends Controller
 
     public function register_process(Request $request)
     {
+        $validated = $request->validate([
+            'name'      => ['required'],
+            'email'     => ['required', 'email', 'unique:users'],
+            'password'  => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised(),
+            ]
+        ]);
+
         try {
-            // $validated = request()->validate([
-            //     'name' => 'required',
-            //     'email' => 'required|email|unique:users',
-            //     'password' => 'required|min:8'
-            // ]);
-
-            // $response = $this->authService->register($validated);
-            $response =  User::create([
-                'name' => $request['name'],
-                'email' => $request['email'],
-                'password' => Hash::make($request->password),
-            ]);
-
+            $response = $this->authService->register($validated);
             if (!$response) {
                 return redirect()->back()->with('error', 'Registrasi gagal');
             }
 
             return redirect()->route('login')->with('success', 'Registrasi berhasil');
         } catch (\Throwable $th) {
-            Log::error("Failted register user", [
-                'line' => $th->getLine(),
-                'file' => $th->getFile(),
-                'message' => $th->getMessage()
+            Log::error([
+                'line'      => $th->getLine(),
+                'file'      => $th->getFile(),
+                'message'   => $th->getMessage()
             ]);
+
+            return redirect()->back()->with('error', 'Terjadi kesalahan');
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email'     => ['required', 'email', 'exists:users'],
+            'password'  => ['required']
+        ], [
+            'email.required' => 'Email wajib diisi',
+            'email.email'    => 'Email tidak valid',
+            'email.exists'    => 'Email tidak terdaftar',
+            'password.required' => 'Password wajib diisi'
+        ]);
+
+        try {
+            $response = $this->authService->login($validated);
+
+            if (!$response) {
+                return redirect()->back()->with('error', 'Kredensial tidak valid!');
+            }
+            return redirect('/panel-control')->with('success', 'Login berhasil');
+        } catch (\Throwable $th) {
+            Log::error([
+                'line'      => $th->getLine(),
+                'file'      => $th->getFile(),
+                'message'   => $th->getMessage(),
+            ]);
+
+            return redirect()->back()->with('error', 'Terjadi kesalahan');
         }
     }
 }
